ce488fda4f88e3d9eb1f0813e577329cb642191b
bao <brayan.allano@gmail.com>
2022-02-09 16:59:35 +0100

    forms

--- controleaveugle_code/src/app/app.module.ts
+++ controleaveugle_code/src/app/app.module.ts
@@ -1,25 +1,21 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ReactiveFormsModule } from "@angular/forms";
+import { FormsModule,ReactiveFormsModule } from "@angular/forms";
 import { PagesModule } from "./pages/pages.module";
-<<<<<<< HEAD
-import { LoginComponent } from './pages/login/login.component';
-=======
 import { DashboardWithAccountComponent } from './pages/dashboard-with-account/dashboard-with-account.component';
 import { PlaylistSearchComponent } from './pages/playlist-search/playlist-search.component';
 import { ListPlaylistComponent } from './pages/list-playlist/list-playlist.component';
 import { PartieComponent } from './partie/partie.component';
 import { ResultsComponent } from './pages/results/results.component';
 import { HeaderComponent } from './pages/header/header.component';
->>>>>>> b89d14942a0a598405004fb33565d280388c3bf5
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
     DashboardWithAccountComponent,
     PlaylistSearchComponent,
     ListPlaylistComponent,
@@ -31,9 +27,13 @@ import { HeaderComponent } from './pages/header/header.component';
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule,
-    PagesModule
+    FormsModule,
+    PagesModule,
+    BrowserAnimationsModule,
+    
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppModule { }
--- controleaveugle_code/src/app/pages/pages.module.ts
+++ controleaveugle_code/src/app/pages/pages.module.ts
@@ -1,15 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HeaderComponent } from './header/header.component';
+import { RegisterComponent } from './register/register.component';
+import { AngularMaterialModule } from '../angular-material.module';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
 
 
 
 @NgModule({
   declarations: [
+    RegisterComponent,
+    LoginComponent,
+    HomeComponent
 
   ],
   imports: [
     CommonModule,
+    AngularMaterialModule,
+    FlexLayoutModule
+    
      ]
 })
 export class PagesModule { }
