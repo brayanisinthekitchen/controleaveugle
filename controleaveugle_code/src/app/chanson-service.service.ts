3358c392b1196c8eb44e5cc8a9a0e01548cd22ba
REMI ROCHET <rereroro034@gmail.com>
2022-02-15 16:54:17 +0100

    samarshpa

--- controleaveugle_code/src/app/chanson-service.service.ts
+++ controleaveugle_code/src/app/chanson-service.service.ts
@@ -23,10 +23,10 @@ export class ChansonServiceService {
     const headers = new HttpHeaders({'Content-Type':'application/json;' , 'Authorization':'Bearer BQBl-WsT53_WTjmOyA-VoPqXZ8IyhnOMnFv430rGY6YH3HU0VytwHI9DevwZRFncST-ihyKD3RSyOcvxxKhVeyVIPeb_XYRx0JIywPe9bAxbnTqd9PuF-haqbUxvmxbLCGRe0gh5eZagby16dlS8T-vLkuHwQ7603ob-a6Dtt7Judhp6'});
     var params:String[] = param.split('/');
     var id = params.pop();
-    var res = this.http.get<Config>("https://api.spotify.com/v1/playlists/"+ id +"/tracks", {headers}).subscribe(data => retour = {
-      items : (data as any)
+    this.http.get<Config>("https://api.spotify.com/v1/playlists/"+ id +"/tracks", {headers, observe:'response'}).subscribe(data => retour = {
+      items : (data as any).items
     });
-    console.log(res);
+    console.log(retour);
   }
 
 }
--- controleaveugle_code/src/app/config.ts
+++ controleaveugle_code/src/app/config.ts
@@ -1,4 +1,4 @@
 
 export interface Config{
-  items : any;
+  items : String[];
 }
